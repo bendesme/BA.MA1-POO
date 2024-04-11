3dfc49a033750308c2dbf554610309214b62795e
Bendo <Bendo@PC-Bendo.local>
2024-04-11 02:21:01 +0200

    Version Class

--- machine_libre.py
+++ machine_libre.py
@@ -1,4 +1,27 @@
+import requests
 import json
+
+# Définition de la classe parent "Livre"
+class Livre:
+    def __init__(self, titre, auteur, isbn, quantite, editeur, prix):
+        self.titre = titre
+        self.auteur = auteur
+        self.isbn = isbn
+        self.quantite = quantite
+        self.editeur = editeur
+        self.prix = prix
+
+    def __str__(self):
+        return f"{self.titre} par {self.auteur}"
+
+# Définition de la classe enfant "Manga" qui hérite de la classe "Livre"
+class Manga(Livre):
+    def __init__(self, titre, auteur, isbn, quantite, editeur, prix, genre):
+        super().__init__(titre, auteur, isbn, quantite, editeur, prix)
+        self.genre = genre
+
+    def __str__(self):
+        return f"{self.titre} par {self.auteur} (Genre: {self.genre})"
 
 # Fonction principale pour poser la question et gérer les réponses
 def interaction_première():
@@ -46,8 +69,8 @@ def Interaction_employer():
         rechercher_livre()
     elif choix == '2':
         encaisser_livre()
-#    elif choix == '3':
-#        ajouter_livre()
+    elif choix == '3':
+        ajouter_livre()
     elif choix == '4':
         modifier_livre()
     elif choix == '5':
@@ -58,38 +81,33 @@ def Interaction_employer():
         print("Option invalide. Veuillez choisir une option valide : 1, 2, 3, 4, 5 ou 6 : ")
         Interaction_employer()
 
-#RECHERCHER UN LIVRE
-def rechercher_livre():
+#                   1. RECHERCHER UN LIVRE
+# Interaction pour rechercher un livre
+def rechercher_livre(livres):
     print("\nRecherche de livre :")
-
-    # Charger le contenu du fichier JSON dans une structure de données Python
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-
-    # Demander à l'utilisateur les critères de recherche
     critere_recherche = input("Entrez le titre, l'auteur, l'ISBN ou l'éditeur du livre que vous recherchez : ").strip().lower()
 
     # Créer une liste pour stocker les résultats de la recherche
     resultats = []
 
-    # Parcourir chaque livre dans la base de données pour vérifier les critères de recherche
-    for livre in data:
-        if critere_recherche in livre['Titre'].lower() \
-            or critere_recherche in livre['Auteur'].lower() \
-            or critere_recherche in livre['ISBN'].lower() \
-            or critere_recherche in livre['éditeur'].lower():
+    # Parcourir chaque livre dans la liste pour vérifier les critères de recherche
+    for livre in livres:
+        if critere_recherche in livre.titre.lower() \
+                or critere_recherche in livre.auteur.lower() \
+                or critere_recherche in livre.isbn.lower() \
+                or critere_recherche in livre.editeur.lower():
             resultats.append(livre)
 
     # Afficher les résultats de la recherche
     if resultats:
         print("\n Résultats de la recherche :")
         for livre in resultats:
-            print(f"\nTitre: {livre['Titre']}")
-            print(f"Auteur: {livre['Auteur']}")
-            print(f"ISBN: {livre['ISBN']}")
-            print(f"Quantité: {livre['quantité']}")
-            print(f"Éditeur: {livre['éditeur']}")
-            print(f"Prix: {livre['prix']}€\n")
+            print(f"\nTitre: {livre.titre}")
+            print(f"Auteur: {livre.auteur}")
+            print(f"ISBN: {livre.isbn}")
+            print(f"Quantité: {livre.quantite}")
+            print(f"Éditeur: {livre.editeur}")
+            print(f"Prix: {livre.prix}€\n")
     else:
         print("\nAucun livre trouvé correspondant à votre recherche.\n")
         print("\n1. effectuer une autre recherche.")
@@ -101,101 +119,92 @@ def rechercher_livre():
             print("\nVous avez quitté le menu.\n")
         else:
             print("\nOption invalide\n")
+        
+    return resultats
 
-#ENCAISSER UN LIVRE
+#                   2. ENCAISSER UN LIVRE
 # Fonction pour rechercher un livre par titre
-def rechercher_livre_par_titre(titre_recherche):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
+def rechercher_livre_par_titre(titre_recherche, livres):
+    for livre in livres:
+        if titre_recherche.lower() in livre.titre.lower():
+            return livre
+    return None
 
-    for livre in data:
-        if titre_recherche.lower() in livre['Titre'].lower():
-            return livre, data
-
-    return None, None
-
-# Fonction pour encaisser un livre
-def encaisser_livre():
+# Interaction pour encaisser un livre
+def encaisser_livre(livres):
     print("\nEncaissement d'un livre :")
     titre = input("Entrez le titre du livre à encaisser : ")
 
-    livre, data = rechercher_livre_par_titre(titre)
+    livre = rechercher_livre_par_titre(titre, livres)
 
-    if livre and data:
-        print("\n\nEst-ce bien ce livre que vous voulez encaisser ?")
-        print(f"\nTitre: {livre['Titre']}")
-        print(f"Auteur: {livre['Auteur']}")
-        print(f"ISBN: {livre['ISBN']}")
-        print(f"Quantité: {livre['quantité']}")
-        print(f"Éditeur: {livre['éditeur']}")
-        print(f"Prix: {livre['prix']}€\n")
+    if livre:
+        print(f"\n\nEst-ce bien ce livre que vous voulez encaisser ? {livre}")
         print("1. Oui")
         print("2. Non")
         print("3. Quitter le menu")
-        
+
         choix = input("Choisissez une option : 1, 2 ou 3 : ")
         if choix == '1':
-            if livre['quantité'] > 0:
-                    livre['quantité'] -= 1
-                    print(f"\nUn exemplaire de '{livre['Titre']}' a été encaissé. Nouvelle quantité : {livre['quantité']}")
-                    print(f"\nprix à payer pour ce livre : '{livre['prix']}€")
-                    if livre['quantité'] == 0:
-                         print("\nAttention : Ce livre est maintenant Sold Out.\n")
-                    # mettre à jour le fichier Json
-                    with open('data.json', 'w') as f:
-                        json.dump(data, f, indent=4)
+            if livre.quantite > 0:
+                livre.quantite -= 1
+                print(f"\nUn exemplaire de '{livre.titre}' a été encaissé. Nouvelle quantité : {livre.quantite}")
+                print(f"\nPrix à payer pour ce livre : '{livre.prix}€")
+                if livre.quantite == 0:
+                    print("\nAttention : Ce livre est maintenant Sold Out.\n")
             else:
                 print("\nDésolé, ce livre est déjà Sold Out.\n")
         elif choix == '2':
-            encaisser_livre()
+            encaisser_livre(livres)
         elif choix == '3':
             print("\nVous avez quitté le menu.\n")
         else:
             print("\nOption invalide. Veuillez recommencer.\n")
-            encaisser_livre()
+            encaisser_livre(livres)
     else:
         print(f"Le livre avec le titre '{titre}' n'a pas été trouvé dans la base de données.")
         print("\n1. effectuer une autre recherche.")
         print("2. Quitter le menu.\n")
         choix = input("Veuillez choisir une option : 1 ou 2 : ")
         if choix == '1':
-            encaisser_livre()
+            encaisser_livre(livres)
         elif choix == '2' :
             print("\nVous avez quitté le menu.\n")
         else:
             print("\nOption invalide\n")
 
-#AJOUTER UN LIVRE
-"""
-def recherche_isbn(self):
-	isbn = input("Veuillez entrer un numéro ISBN : ")
+#                       3. AJOUTER UN LIVRE
+# Fonction pour rechercher un livre par ISBN sur Open Library
+def recherche_isbn():
+    isbn = input("Veuillez entrer un numéro ISBN : ")
 
-	url = f"https://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&format=json&jscmd=data"
+    url = f"https://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&format=json&jscmd=data"
+    response = requests.get(url)
+    data = response.json()
 
-	response = requests.get(url)
-	data = response.json()
+    if f"ISBN:{isbn}" in data:
+        # Extraire les informations nécessaires du résultat de l'API
+        livre_info = data[f"ISBN:{isbn}"]
+        title = livre_info['title']
+        authors = livre_info['authors'][0]['name'] if 'authors' in livre_info else 'Inconnu'
+        publish_date = livre_info['publish_date'] if 'publish_date' in livre_info else 'Inconnue'
+        publishers = livre_info['publishers'][0]['name'] if 'publishers' in livre_info else 'Inconnu'
 
-		# Extraire les informations nécessaires du résultat de l'API
-	title = data[f"ISBN:{isbn}"]['title']
-	author = data[f"ISBN:{isbn}"]['authors'][0]['name']
-	year=data[f"ISBN:{isbn}"]['publish_date']
-	maison_ed=data[f"ISBN:{isbn}"]['publishers'][0]["name"]
+        # Demander à l'utilisateur de saisir la quantité et le prix
+        quantity = int(input(f"Quantité en stock pour {title} : "))
+        price = float(input(f"Prix unitaire (en €) pour {title} : "))
 
-		# Demander à l'utilisateur de saisir la quantité et le prix
-	quantity = int(input(f"Quantité en stock pour {title} : "))
-	price = float(input(f"Prix unitaire (en €) pour {title} : "))
-	articlenouv=Livre(isbn,title,price,author,maison_ed,year,None,"ND",isbn,quantity,None)
+        return title, authors, publish_date, publishers, isbn, quantity, price
+    else:
+        print("Aucune information trouvée pour cet ISBN.")
 
-		# Ajouter l'article au stock
-	self.articlesList.append(articlenouv)
-	print(f"Article ajouté de OpenLibrary : {title} de {author}")
-
+# Fonction pour ajouter un livre
 def ajouter_livre():
-    print("Voulez-vous ajouter un livre manuellement ou depuis une recherche Openlibrary ?")
+    print("Voulez-vous ajouter un livre manuellement ou depuis une recherche Open Library ?")
     print("1. Manuellement")
-    print("2. Depuis une recherche Openlibrary")
-    choix = input("veuillez choisir une option : 1 ou 2 : ")
-    if choix == '1':   
+    print("2. Depuis une recherche Open Library")
+    choix = input("Veuillez choisir une option : 1 ou 2 : ")
+
+    if choix == '1':
         print("Ajout d'un nouveau livre :")
         titre = input("Titre du livre : ")
         auteur = input("Auteur du livre : ")
@@ -203,136 +212,131 @@ def ajouter_livre():
         quantite = int(input("Quantité disponible : "))
         editeur = input("Éditeur du livre : ")
         prix = float(input("Prix du livre : "))
-    elif choix == '2' :
-        recherche_isbn()
+
+        try:
+            # Charger le contenu du fichier JSON existant
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            # Si le fichier n'existe pas, initialiser une nouvelle liste
+            data = []
+
+        # Ajouter le nouveau livre à la liste existante
+        data.append(nouveau_livre.__dict__)
+
+        # Écrire les données mises à jour dans le fichier JSON
+        try:
+            with open('data.json', 'w') as f:
+                json.dump(data, f, indent=4)
+            print("Livre ajouté avec succès !")
+        except Exception as e:
+            print("Erreur lors de l'écriture dans le fichier JSON :", e)
+        return nouveau_livre
+
+    elif choix == '2':
+        # Recherche d'un livre par ISBN sur Open Library
+        livre_info = recherche_isbn()
+
+        if livre_info:
+            # Création d'un nouvel objet Livre
+            nouveau_livre = Livre(*livre_info)
+            print("Livre ajouté avec succès !")
+            return nouveau_livre
+
     else:
         print("\nOption invalide\n")
-        
-    nouveau_livre = {
-        "ID": len(data) + 1,  # Générer un nouvel ID en fonction de la longueur actuelle de la liste
-        "Titre": titre,
-        "Auteur": auteur,
-        "ISBN": isbn,
-        "quantité": quantite,
-        "éditeur": editeur,
-        "prix": prix
-    }
 
-    # Charger le contenu du fichier JSON existant
-    with open('data.json', 'r') as f:
-        data = json.load(f)
+    return None
 
-    # Ajouter le nouveau livre à la liste existante
-    data.append(nouveau_livre)
-
-    # Écrire les données mises à jour dans le fichier JSON
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
-
-    print("Livre ajouté avec succès !")
-"""
-
-#MODIFIER UN LIVRE
-def modifier_livre():
+#                           4. MODIFIER UN LIVRE
+def modifier_livre(livres):
     print("Liste des titres des livres :")
 
-    # Charger le contenu du fichier JSON dans une structure de données Python
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-
     # Afficher la liste des titres des livres
-    for livre in data:
-        print(f"ID: {livre['ID']}, Titre: {livre['Titre']}")
+    for livre in livres:
+        print(f"Titre: {livre.titre}")
 
-    # Demander à l'utilisateur de choisir l'ID du livre à modifier
-    id_livre = input("Entrez l'ID du livre à modifier : ")
+    # Demander à l'utilisateur de choisir le titre du livre à modifier
+    titre_livre = input("Entrez le titre du livre à modifier : ")
 
-    # Rechercher le livre par ID
-    livre_a_modifier = None
-    for livre in data:
-        if str(livre['ID']) == id_livre:
-            livre_a_modifier = livre
-            break
+    # Rechercher le livre par titre
+    livre_a_modifier = rechercher_livre_par_titre(titre_livre, livres)
 
     # Si le livre est trouvé, permettre à l'utilisateur de modifier ses informations
     if livre_a_modifier:
-        print(f"Livre trouvé : {livre_a_modifier['Titre']}")
+        print(f"Livre trouvé : {livre_a_modifier}")
+
+        # Demander les nouvelles informations à l'utilisateur
         nouveau_titre = input("Entrez le nouveau titre (laissez vide pour ne pas modifier) : ").strip()
         nouveau_auteur = input("Entrez le nouvel auteur (laissez vide pour ne pas modifier) : ").strip()
-        nouveau_ISBN = input("Entrez le nouvel ISBN (laissez vide pour ne pas modifier) : ").strip()
+        nouveau_isbn = input("Entrez le nouvel ISBN (laissez vide pour ne pas modifier) : ").strip()
         nouvelle_quantite = input("Entrez la nouvelle quantité (laissez vide pour ne pas modifier) : ").strip()
         nouveau_editeur = input("Entrez le nouvel éditeur (laissez vide pour ne pas modifier) : ").strip()
         nouveau_prix = input("Entrez le nouveau prix (laissez vide pour ne pas modifier) : ").strip()
 
         # Mettre à jour les informations du livre si l'utilisateur a fourni de nouvelles valeurs
         if nouveau_titre:
-            livre_a_modifier['Titre'] = nouveau_titre
+            livre_a_modifier.titre = nouveau_titre
         if nouveau_auteur:
-            livre_a_modifier['Auteur'] = nouveau_auteur
-        if nouveau_ISBN:
-            livre_a_modifier['ISBN'] = nouveau_ISBN
+            livre_a_modifier.auteur = nouveau_auteur
+        if nouveau_isbn:
+            livre_a_modifier.isbn = nouveau_isbn
         if nouvelle_quantite:
-            livre_a_modifier['quantité'] = int(nouvelle_quantite)
+            livre_a_modifier.quantite = int(nouvelle_quantite)
         if nouveau_editeur:
-            livre_a_modifier['éditeur'] = nouveau_editeur
+            livre_a_modifier.editeur = nouveau_editeur
         if nouveau_prix:
-            livre_a_modifier['prix'] = float(nouveau_prix)
+            livre_a_modifier.prix = float(nouveau_prix)
 
-        # Enregistrer les modifications dans le fichier JSON
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=4)
         print("Livre modifié avec succès.")
     else:
-        print(f"Aucun livre trouvé avec l'ID '{id_livre}'.")
+        print(f"Aucun livre trouvé avec le titre '{titre_livre}'.")
         print("\n1. effectuer une autre recherche.")
         print("2. Quitter le menu.\n")
         choix = input("Veuillez choisir une option : 1 ou 2 : ")
         if choix == '1':
-            modifier_livre()
+            modifier_livre(livres)
         elif choix == '2' :
             print("\nVous avez quitté le menu.\n")
         else:
             print("\nOption invalide\n")
 
-#SUPPRIMER UN LIVRE
-def supprimer_livre():
+# Fonction pour supprimer un livre
+def supprimer_livre(livres):
     print("Liste des titres des livres :")
 
-    # Charger le contenu du fichier JSON dans une structure de données Python
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-
     # Afficher la liste des titres des livres
-    for livre in data:
-        print(f"ID: {livre['ID']}, Titre: {livre['Titre']}")
+    for livre in livres:
+        print(f"Titre: {livre.titre}")
 
-    # Demander à l'utilisateur de choisir l'ID du livre à supprimer
-    id_livre = input("Entrez l'ID du livre à supprimer : ")
+    # Demander à l'utilisateur de choisir le titre du livre à supprimer
+    titre_livre = input("Entrez le titre du livre à supprimer : ")
 
-    # Rechercher le livre par ID
-    livre_a_supprimer = None
-    for livre in data:
-        if str(livre['ID']) == id_livre:
-            livre_a_supprimer = livre
-            break
+    # Rechercher le livre par titre
+    livre_a_supprimer = rechercher_livre_par_titre(titre_livre, livres)
 
     # Si le livre est trouvé, demander confirmation à l'utilisateur avant de le supprimer
     if livre_a_supprimer:
-        print(f"Livre trouvé : {livre_a_supprimer['Titre']}")
+        print(f"Livre trouvé : {livre_a_supprimer}")
         confirmation = input("Voulez-vous vraiment supprimer ce livre ? (o/n) : ").strip().lower()
 
         if confirmation == 'o' or confirmation == 'oui':
             # Supprimer le livre de la liste
-            data.remove(livre_a_supprimer)
-
-            # Enregistrer les modifications dans le fichier JSON
-            with open('data.json', 'w') as f:
-                json.dump(data, f, indent=4)
+            livres.remove(livre_a_supprimer)
             print("Livre supprimé avec succès.")
         else:
             print("Suppression annulée.")
     else:
-        print(f"Aucun livre trouvé avec l'ID '{id_livre}'.")
+        print(f"Aucun livre trouvé avec le titre '{titre_livre}'.")
+        print("\n1. effectuer une autre recherche.")
+        print("2. Quitter le menu.\n")
+        choix = input("Veuillez choisir une option : 1 ou 2 : ")
+        if choix == '1':
+            supprimer_livre(livres)
+        elif choix == '2' :
+            print("\nVous avez quitté le menu.\n")
+        else:
+            print("\nOption invalide\n")
 
 # Appel de la fonction principale pour démarrer l'interaction avec le client
 if __name__ == "__main__":
